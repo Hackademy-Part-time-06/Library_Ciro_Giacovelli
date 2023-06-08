4b59e2afbe46f1898b031f59663fe723e52e21f3
Giacovelli Ciro Francesco <ciro254@gmail.com>
2023-06-08 19:27:33 +0200

    correzioni varie

--- Library/app/Http/Controllers/AuthorController.php
+++ Library/app/Http/Controllers/AuthorController.php
@@ -8,10 +8,7 @@ use App\Http\Requests\UpdateAuthorRequest;
 
 class AuthorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-
+   
     public function __construct()
     {
         $this->middleware('auth')->only('create', 'edit');
@@ -23,17 +20,13 @@ class AuthorController extends Controller
         return view ('index', ['author' => $author]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+   
     public function create()
     {
         return view('authors.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+   
     public function store(StoreAuthorRequest $request)
     {
         Author::create([
@@ -45,26 +38,20 @@ class AuthorController extends Controller
         return redirect()->route('authors.index')->with('success', 'Creazione avvenuta con successo!');
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Author $author)
     {
         return view('authors.show', compact('author'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(Author $author)
     {
         return view('authors.edit', compact('author'));
 
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(UpdateAuthorRequest $request, Author $author)
     {
         $author->update([
@@ -76,9 +63,6 @@ class AuthorController extends Controller
         return redirect()->route('authors.index')->with('success', 'Modifica avvenuta con successo!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Author $author)
     {
         $author->delete();
--- Library/database/migrations/2023_05_30_182259_add_to_books_table.php
+++ Library/database/migrations/2023_05_30_182259_add_to_books_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('books', function (Blueprint $table) {
-            $table->string('image')->after('author')->nullable;
+            $table->string('image')->after('years')->nullable;
         });
     }
 
